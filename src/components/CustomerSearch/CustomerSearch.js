91ba51df233b89b3fe8d980caf1592c737fee8c3
Lucas Robitaille <lucrobitaille@ymail.com>
2020-02-19 10:27:04 -0600

    created initial component structure

---
+++ src/components/CustomerSearch/CustomerSearch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const customerSearch = (props) => (
+    <main>
+        <form>
+            <input type='text'/>
+            <button type='submit'>Search</button>
+        </form>
+    </main>
+)
+
+export default customerSearch
